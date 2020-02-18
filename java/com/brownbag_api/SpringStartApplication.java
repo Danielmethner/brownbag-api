2f604b9ddcf058b2ff41321eb2013a3e3b11adcd
Daniel <Daniel@LAPTOP-IM14107M>
2020-02-18 23:55:16 +0800

    figaro spring restructuring

--- java/com/brownbag_api/SpringStartApplication.java
+++ java/com/brownbag_api/SpringStartApplication.java
@@ -3,6 +3,7 @@ package com.brownbag_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import com.brownbag_api.model.demo.Demo;
 @RestController
 @EnableAutoConfiguration
 @SpringBootApplication
-public class SpringStartApplication {
+public class SpringStartApplication  extends SpringBootServletInitializer{
 
 	@RequestMapping("/")
 	String home() {
