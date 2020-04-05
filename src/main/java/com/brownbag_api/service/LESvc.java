a280a0af9a75cdedf8b3378f4c070b3eafd10059
OneCentral <danielmethnerek@gmail.com>
2020-04-05 17:03:43 +0800

    Refactoring of LESvc

--- src/main/java/com/brownbag_api/service/LESvc.java
+++ src/main/java/com/brownbag_api/service/LESvc.java
@@ -23,14 +23,22 @@ public class LESvc {
 
 	@Autowired
 	private LERepo lERepo;
-		
+
 	@Autowired
 	private PosSvc posSvc;
-	
-	public LegalEntity createLE(ELE eOrg, User manager, ELEType legalEntityType, boolean addMacc) {
-		LegalEntity le = new LegalEntity(eOrg.toString(), manager, legalEntityType);
+
+	/**
+	 * 
+	 * @param eOrg
+	 * @param user - managing user
+	 * @param legalEntityType - Legal or natural person
+	 * @param addMacc - create MACC for newly created Legal Entity
+	 * @return
+	 */
+	public LegalEntity createLE(ELE eOrg, User user, ELEType legalEntityType, boolean addMacc) {
+		LegalEntity le = new LegalEntity(eOrg.toString(), user, legalEntityType);
 		le = lERepo.save(le);
-		if (addMacc) { 
+		if (addMacc) {
 			posSvc.createMacc(0, le, 0);
 		}
 		return le;
@@ -38,25 +46,20 @@ public class LESvc {
 
 	public LegalEntity getNaturalPerson(User manager) {
 		List<LegalEntity> lEs = lERepo.findByUserAndLegalEntityType(manager, ELEType.PERSON_NATURAL);
-		if (!lEs.isEmpty()) {
-			LegalEntity natPerson = lEs.get(0);
-			return natPerson;
-		}
-
-		return null;
+		return lEs.isEmpty() ? null : lEs.get(0);
 	}
 
 	public void createNaturalPerson(User user) {
 		if (getNaturalPerson(user) == null) {
 			LegalEntity natPerson = new LegalEntity(user.getName(), user, ELEType.PERSON_NATURAL);
 			lERepo.save(natPerson);
-			posSvc.createMacc(0, natPerson, 0);			
+			posSvc.createMacc(0, natPerson, 0);
 		}
 	}
 
 	public Position getMacc(LegalEntity le) {
 		return posSvc.findByOwnerAndIsMacc(le, true);
-		
+
 	}
 
 }
