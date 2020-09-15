7d5f9ec6178a43e2e4ef81b7f4a1c99a8f798f36
OneCentral <daniel.methner@pulsar-sd.com>
2020-09-15 16:38:00 +0800

    Rename standard user

--- src/main/java/com/brownbag_api/model/enums/EUser.java
+++ src/main/java/com/brownbag_api/model/enums/EUser.java
@@ -3,7 +3,7 @@ package com.brownbag_api.model.enums;
 public enum EUser {
 	MGR_ECB("European Central Bank", ERole.ROLE_ORG), MGR_DEUTSCHE_BANK("Deutsche Bank", ERole.ROLE_MGR),
 	MGR_GOVERNMENT("Government", ERole.ROLE_ORG), MGR_BROKER("Broker", ERole.ROLE_BROKER),
-	U_TRADER_1("Trader 1", ERole.ROLE_MGR), U_TRADER_2("Trader 2", ERole.ROLE_MGR),
+	U_TRADER_1("John Doe", ERole.ROLE_MGR), U_TRADER_2("Jane Dawson", ERole.ROLE_MGR),
 	U_EOP("End of Year processor", ERole.ROLE_GLOBAL_ADMIN);
 
 	public final String name;
